aa1cb48b94f88685e0c37e1866d0db3df832a25c
Amdjed <amdjed.kaabi@univ-constantine2.dz>
2025-01-08 14:39:21 +0100

    fixes

--- access.sh
+++ access.sh
@@ -5,13 +5,13 @@ aws configure
 aws eks update-kubeconfig --region "us-east-1" --name "main-cluster"
 
 # ArgoCD Access
-# argo_url=$(kubectl get svc -n argocd | grep argocd-server | awk '{print$4}' | head -n 1)
-# argo_initial_password=$(argocd admin initial-password -n argocd)
+argo_url=$(kubectl get svc -n argocd | grep argocd-server | awk '{print$4}' | head -n 1)
+argo_initial_password=$(argocd admin initial-password -n argocd)
 
-# ArgoCD Credentials
-# argo_user="admin"
+ArgoCD Credentials
+argo_user="admin"
 
-# argo_password=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
+argo_password=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
 
 # Prometheus and Grafana URLs and credentials
 prometheus_url=$(kubectl get svc -n prometheus | grep stable-kube-prometheus-sta-prometheus | awk '{print $4}')
@@ -21,9 +21,9 @@ grafana_password=$(kubectl get secret stable-grafana -n prometheus -o jsonpath="
 
 # Print or use these variables
 echo "------------------------"
-# echo "ArgoCD URL: $argo_url"
-# echo "ArgoCD User: $argo_user"
-# echo "ArgoCD Initial Password: $argo_initial_password" | head -n 1
+echo "ArgoCD URL: $argo_url"
+echo "ArgoCD User: $argo_user"
+echo "ArgoCD Initial Password: $argo_initial_password" | head -n 1
 echo
 echo "Prometheus URL: $prometheus_url":9090
 echo
